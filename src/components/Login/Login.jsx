79295475faaba848b22b6046ee192aabf42f9e13
HamidReza <delshad.swdn@gmail.com>
2022-12-30 14:31:25 +0100

    add some style

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useCookies } from "react-cookie";
 import { useForm } from "react-hook-form";
-import { LockClosedIcon } from "@heroicons/react/20/solid";
+import { ReactComponent as Image } from "../../assets/pic.svg";
 
 export default function Login() {
   const [userName, setUserName] = useState("");
@@ -14,7 +14,7 @@ export default function Login() {
     handleSubmit,
     formState: { errors },
   } = useForm();
-  // const onSubmit = (data) => console.log(data);
+
   useEffect(() => {
     if (cookies.token) {
       setIsLogin(true);
@@ -52,12 +52,10 @@ export default function Login() {
     })
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
         if (data.accessToken?.length > 0) {
           handle(data.accessToken);
           setIsLogin(true);
         } else if (data.error) {
-          console.log(data);
           console.log(data.error);
           setIsUser(false);
         }
@@ -80,47 +78,52 @@ export default function Login() {
           </button>
         </div>
       ) : (
-        <div className="flex flex-col gap-5 items-center bg-white p-10 rounded-lg mt-6 shadow-lg">
-          <h3 className="text-center text-2xl font-bold tracking-tight text-gray-900">
-            Sign in to your account
-          </h3>
-          <form
-            onSubmit={handleSubmit(submitBtn)}
-            className="flex flex-col gap-5 w-[250px]"
-          >
-            <input
-              {...register("userName", {
-                required: true,
-                maxLength: 20,
-              })}
-              type="text"
-              placeholder="Email"
-              onChange={userNameHandler}
-              className="rounded-md"
-            />
-            {errors.userName && (
-              <span className="text-red-500">Email is required!</span>
-            )}
-            <input
-              {...register("password", { required: true })}
-              type="password"
-              placeholder="Password"
-              onChange={passwordHandler}
-              className="rounded-md"
-            />
-            {errors.password && (
-              <span className="text-red-500">Password is required!</span>
-            )}
+        <div className="flex rounded-lg mt-6 shadow-lg overflow-hidden bg-white">
+          <div className="w-[500px] ">
+            <Image />
+          </div>
+          <div className="flex flex-col gap-5 items-center p-10">
+            <h3 className="text-center text-2xl font-bold tracking-tight text-gray-900">
+              Sign in to your account
+            </h3>
+            <form
+              onSubmit={handleSubmit(submitBtn)}
+              className="flex flex-col gap-5 w-[250px]"
+            >
+              <input
+                {...register("userName", {
+                  required: true,
+                  maxLength: 20,
+                })}
+                type="text"
+                placeholder="Email"
+                onChange={userNameHandler}
+                className="rounded-md"
+              />
+              {errors.userName && (
+                <span className="text-red-500">Email is required!</span>
+              )}
+              <input
+                {...register("password", { required: true })}
+                type="password"
+                placeholder="Password"
+                onChange={passwordHandler}
+                className="rounded-md"
+              />
+              {errors.password && (
+                <span className="text-red-500">Password is required!</span>
+              )}
 
-            <input
-              type="submit"
-              className="bg-green-500 py-2 rounded-md text-white"
-              value="Log in"
-            />
-          </form>
-          {!isUser ? (
-            <p className="text-red-500">Invalid username or password!</p>
-          ) : null}
+              <input
+                type="submit"
+                className="bg-[#9F59F5] py-2 rounded-md text-white"
+                value="Log in"
+              />
+            </form>
+            {!isUser ? (
+              <p className="text-red-500">Invalid username or password!</p>
+            ) : null}
+          </div>
         </div>
       )}
     </div>
